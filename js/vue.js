44f6c0727199e7d152fbccf841f7bd73dd3f940d
Coyote Codornices Marin (they/them) <coyote@coyote.rocks>
2023-02-12 17:13:48 -0800

    double mana now has 10% chance of resetting to 0

--- js/vue.js
+++ js/vue.js
@@ -11,9 +11,15 @@ function loadVue() {
       addOneToMana () {
         this.player.mana += 1
       },
-      doubleMana () {
-        this.player.mana *= 2
-      },
+      doubleOrResetMana () {
+      	// Math.random() is a number between 0.0 and 1.0
+      	if (Math.random() < 0.1) {
+      		// 10% of clearing mana
+      		this.player.mana = 0
+      	} else {
+      		this.player.mana *= 2
+      	}
+      }
     },
   }).mount('#app')
 }
